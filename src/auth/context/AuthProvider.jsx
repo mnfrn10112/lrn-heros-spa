3c5fb3afd9327322766f1329a266ae301d35695b
mnfrn10112 <manfran10112@gmail.com>
2023-01-03 17:49:18 -0500

    save user on localstorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,22 +4,29 @@ import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 
 
-const initialState = {
-    logged: false,
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return {
+        logged: !!user,
+        user: user
+    };
 };
 
+
 export const AuthProvider = ({ children }) => {
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, {}, init);
 
     // const login = async (email, password) => {
 
     // };
 
     const login = (name) => {
+        const user = { id: 123, name };
         const action = {
             type: types.login,
-            payload: { id: 123, name }
+            payload: user
         };
+        localStorage.setItem('user', JSON.stringify(user));
         dispatch(action);
     };
 
