fecfb354a86a82db3008add143647e01d4723aaf
Manuel Gil <manuel.gil@services.mss.pe>
2023-01-02 21:52:18 -0500

    add internal hero routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
-import { DCPage, MarvelPage } from "../heroes";
+import { DCPage, HeroesRoutes, MarvelPage } from "../heroes";
 import { Navbar } from "../ui";
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route path="/marvel" element={<MarvelPage />} />
-                <Route path="/dc" element={<DCPage />} />
-
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/*" element={<Navigate to='/marvel' />} />
-
+                <Route path="/*" element={<HeroesRoutes />} />
             </Routes>
         </>
     );
