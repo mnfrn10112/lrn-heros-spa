363070efa04b36417b3c76770c5aabfc667544da
mnfrn10112 <manfran10112@gmail.com>
2023-01-03 10:28:29 -0500

    useLocation and useNavigation

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,24 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components/";
+import queryString from 'query-string';
+
 
 export const SearchPage = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { q = '' } = queryString.parse(location.search);
+
+    const { searchText, onInputChange } = useForm({
+        searchText: ''
+    });
+
+    const handleSumit = (e) => {
+        e.preventDefault();
+        if (searchText.trim().length <= 1) return;
+        navigate(`?q=${searchText}`);
+    };
+
     return (<>
         <h1>SearchPage</h1>
         <hr />
@@ -8,18 +26,16 @@ export const SearchPage = () => {
             <div className="col-5">
                 <h4>Searching</h4>
                 <hr />
-                <form >
+                <form onSubmit={handleSumit} >
                     <input type="text"
                         placeholder="Search a hero"
                         className="form-control"
                         name="searchText"
                         autoComplete="off"
-                    />
-
-                    <button type="button" className="btn btn-outline-primary mt-1">Search</button>
-
+                        value={searchText}
+                        onChange={onInputChange} />
+                    <button type="submit" className="btn btn-outline-primary mt-1">Search</button>
                 </form>
-
             </div>
             <div className="col-7">
                 <h4>Results</h4>
@@ -27,7 +43,7 @@ export const SearchPage = () => {
                     Search hero
                 </div>
                 <div className="alert alert-danger">
-                    No hero with <b>ABC</b>
+                    No hero with <b>{q}</b>
                 </div>
                 <HeroCard />
             </div>
